313a16f00eae5d44204c2cb9fb872b59352536a5
DevT045T <info@t045t.dev>
2024-11-07 19:11:20 +0100

    Initial commit :)

---
+++ src/API.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Class API
+ * 
+ * This class serves as a basic foundation for a REST API. It can be extended to handle various
+ * HTTP methods like GET, POST, PUT, DELETE, etc., and can be customized to fit your application's needs.
+ * 
+ * @package PHP-API
+ * @author t045t
+ * @link https://t045t.dev
+ * @license MIT
+ */
+class API {
+    
+    /**
+     * @TODO Implement logic to handle incoming API requests and responses.
+     *       Consider handling HTTP methods such as GET, POST, PUT, DELETE, etc.
+     */
+}
